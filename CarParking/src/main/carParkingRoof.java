9be067624a9a6245eb58e38431af6aa4fe154f30
mnamiz <mnamiz@gmail.com>
2021-09-17 16:30:40 +0300

    Comments

--- CarParking/src/main/carParkingRoof.java
+++ CarParking/src/main/carParkingRoof.java
@@ -6,7 +6,7 @@ public class carParkingRoof {
 
 	
 	public static   int n=4; 							// the count of cars
-	public static  int[] cars=new int[] {6,2,12,7}; 	// the parking spots where cars are parked
+	public static  int[] cars=new int[] {6,12,2,7}; 	// the parking spots where cars are parked
 	public static  int k=3; 							//the number of cars that have to be covered by the roof
 	public static  int roofLength; 						// the minimum length of a roof to be calculated that can cover k cars
 	 
@@ -25,7 +25,7 @@ public class carParkingRoof {
 		}
 		
 		System.out.println();
-		System.out.println(roofLength);    				// print the result
+		System.out.println("The shortest roof that meets the requirement is of length: "+roofLength);    				// print the result
 	}
 
 }
